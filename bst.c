4394f28b259943b033436c84b4d969ad5b3f0a3b
Bryan Tsai <twbikeman@gmail.com>
2020-01-14 13:45:42 +0800

    test

---
+++ bst.c
@@ -0,0 +1,33 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+  int key;
+  struct node *left , *right; 
+};
+
+struct node* newNode(int item) {
+  struct node * temp = (struct node *)malloc(sizeof(struct node));
+  temp->key = item;
+  temp->left = temp->right = NULL;
+  return temp;
+}
+
+void inorder(struct node *root) {
+  if (root != NULL) {
+    inorder(root->left);
+    printf("%d \n", root->key);
+    inorder(root->right);
+  }
+}
+
+struct node* insert(struct node *node, int key) {
+  if (node == NULL) return newNode(key);
+  if (key < node->key)
+    node->left = insert(node->left, key);
+    else if (key > node->key)
+      node->right = insert(node->right, key);
+  return node;
+}
+
