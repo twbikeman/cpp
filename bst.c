56a6a2f345a53c537e48a792414bd3588aafe0d9
Bryan Tsai <twbikeman@gmail.com>
2020-01-15 09:55:00 +0800

    test

--- bst.c
+++ bst.c
@@ -28,6 +28,24 @@ struct node* insert(struct node *node, int key) {
     node->left = insert(node->left, key);
     else if (key > node->key)
       node->right = insert(node->right, key);
-  return node;
+  /* return the (unchanged) node pointer */
+  return node; 
+}
+
+
+int main() {
+  struct node *root = NULL;
+  root = insert(root, 50);
+  insert(root, 30);
+  insert(root, 20);
+  insert(root, 40);
+  insert(root, 70);
+  insert(root, 60);
+  insert(root, 80);
+
+  inorder(root);
+  return 0;
+  
+
 }
 
